0b4554850ecbbf1c4d350b81bc80449624d8069f
Phebo Wibbens <phebo.wibbens@insead.edu>
2020-09-28 16:43:12 +0200

    Update scenarios figure

--- run-model.R
+++ run-model.R
@@ -36,7 +36,7 @@ suppressWarnings(dir.create(file.path("output")))
 suppressWarnings(dir.create(file.path("figures")))
 
 saveAppData <- F
-writeFigures <- T
+writeFigures <- F
 selSc <- c("C1 - 1", "C2 - 1", "C3 - 2", "C4 - 3", "C5 - 0", "C6 - 1", "C7 - 1", "C8 - 3", "H1 - 2", "H2 - 2", "H3 - 2")
 
 #### Read and clean data ####
@@ -126,27 +126,18 @@ dfOutSc <- left_join(
   select(iter, group = name, gBase, g) %>% pivot_longer(gBase:g) %>%
   group_by(group, name) %>%
   summarize(estimate = median(value), low = quantile(value, probs=0.025), high = quantile(value, probs=0.975)) %>%
-  ungroup()
+  ungroup() #%>% mutate(name = factor(name, levels = c("g","gBase"), labels = c(expression(italic(g)), expression(italic(g)^(base)))))
 
 #### Make charts ####
 
-ylabels <- dfOutSc %>% filter(group == "Worst 10%") %>% pull(estimate)
-ylabels <- c(ylabels, mean(ylabels)-0.2)
-
-ggplot(dfOutSc) + geom_point(aes(x = group, y = estimate)) +
-  geom_errorbar(aes(x = group, ymin = low, ymax = high), width = 0.7) +
-  geom_segment(aes(x = group, xend = group, y = gBase - 0.25, yend = g + 0.25),
-               data = (~ .x %>% select(group:estimate) %>% pivot_wider(values_from = estimate)),
-               arrow = arrow()) +
-  geom_text(aes(x = 3.8, y = ylabels,
-                label = c("italic(g)", "italic(g)^(base)", "'Average\neffect of\nbasic\npolicies'")),
-            data = tibble(),
-            parse = T) +
-  geom_blank(aes(x= 4.5, y = 1)) +
+ggplot(dfOutSc, aes(x = group, y = estimate, color = name)) + geom_point() +
+  geom_errorbar(aes(x = group, ymin = low, ymax = high), width = 0.6) +
   geom_hline(yintercept = 0) +
-  xlab(element_blank()) + ylab(element_blank())
-if(writeFigures) ggsave(paste0("figures/fig-scenarios.png"), height = 4, width = 3.5)
-
+  xlab("Jurisdiction quantile") + ylab(element_blank()) +
+  scale_color_brewer(labels = c("With core\npolices", "Base"), palette = "Dark2",
+                     guide = guide_legend(reverse = TRUE), name = "Growth rate") +
+  theme(axis.title.x = element_text(vjust = -0.5))
+if(writeFigures) ggsave(paste0("figures/fig-scenarios.png"), height = 4, width = 4)
 
 fGPol <- dfOut %>% filter(name == "dgCum") %>%
   group_by(pol) %>% mutate(estimate = estimate - c(0, estimate[-length(estimate)])) %>%
